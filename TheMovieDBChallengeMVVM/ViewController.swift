2a145508cb73814b398eded19feb853ba9c98b48
Kevin Candia Villagómez <kevin.candia@softtek.com>
2023-08-19 10:33:45 -0500

    Initial Commit

---
+++ TheMovieDBChallengeMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDBChallengeMVVM
+//
+//  Created by Kevin Candia Villagómez on 19/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
